8eea763e602b02c8fdaa233bebaa9b1f1ff997fe
Sergei Kolesnik <sergey.kolesnik.ld@gmail.com>
2025-11-21 03:34:45 +0100

    fixup! refine frontend

--- include/omnirefl/reflected_scope.hpp
+++ include/omnirefl/reflected_scope.hpp
@@ -6,8 +6,32 @@
 #include <type_traits>
 #include <utility>
 
+#if defined(__cpp_lib_variant) && __cpp_lib_variant >= 201606
+#  include <variant>
+#endif
+
 // todo: this header should only utilities available within a reflected scope
 namespace omni {
+
+namespace customization {
+
+template <template <typename...> class>
+struct visit;
+
+#if defined(__cpp_lib_variant) && __cpp_lib_variant >= 201606
+
+template <>
+struct visit<std::variant> {
+  template <typename Visitor, typename... Variants>
+  decltype(auto) operator()(Visitor &&vis, Variants &&...vars) const {
+    return std::visit(std::forward<Visitor>(vis),
+      std::forward<Variants>(vars)...);
+  }
+};
+
+#endif // __cpp_lib_variant
+
+} // namespace customization
 
 namespace detail {
 namespace {
@@ -105,6 +129,8 @@ struct _is_reflected;
 /// todo: use the tool's pass to detect the invalid usage
 /// do not add specializations
 /// (!!!) do not instantiate this outside of a reflected scope
+///
+/// note: T is decayed by generated specialization
 template <typename T, typename = void>
 struct is_reflected;
 
@@ -113,6 +139,9 @@ enum class reflected_entity {
   member, // tagged.field
   enumeration, // enum | enum class
 };
+
+template <template <typename> class, typename>
+struct reflected_binding;
 
 // todo: what about field pointer? `reflected_field<T::name>` is a valid
 // use-case, to get a string "name" for example
@@ -133,15 +162,17 @@ struct reflected_mem_binding {
     return value();
   }
 
+  // todo: if field is a pointer, need to return by value
+  // todo: enable_if is_mutable
   template <typename V>
   constexpr const type &set_value(V &&v) {
-    return Meta::set_value(std::forward<V>(v));
+    return Meta::set_value(bound, std::forward<V>(v));
   }
 };
 
 // reflection for struct | class | union types
 template <typename T>
-struct reflected_tagged final:
+struct reflected_tagged_t final:
     detail::_reflected<typename std::decay<T>::type> {
   static_assert(is_reflected<T>::value,
     "Type was not reflected (Calling outside a reflected scope?)");
@@ -150,22 +181,28 @@ struct reflected_tagged final:
   static_assert(reflected_entity::tagged == meta::entity(),
     "Inconcistent reflection");
 
-  using meta::type; //< yields T
+  using typename meta::type; //< yields T
   using meta::name; //< static constexpr const char(&[N]) name()`
   using meta::fields; //< yields std::tuple of fields' meta types
 
+  // fixme: auto without trailing return is C++14
   static constexpr auto fields(const T &t) noexcept {
     return _fields(t);
   }
 
+  // fixme: auto without trailing return is C++14
   static constexpr auto fields(T &t) noexcept {
     return _fields(t);
   }
+
+  // todo: mutable_fields(T &t) and mutable_fields(const T &t)
 
   private:
   template <typename _T>
   struct _bind_fields_metadata {
     _T &t;
+
+    // fixme: auto without trailing return is C++14
     template <typename... Meta>
     constexpr auto operator()(Meta... field_meta) const noexcept {
       return std::make_tuple(
@@ -173,6 +210,7 @@ struct reflected_tagged final:
     }
   };
 
+  // fixme: auto without trailing return type is C++14
   template <typename _T>
   static constexpr auto _fields(_T &t) noexcept {
     return detail::apply(_bind_fields_metadata<_T>{t}, meta::fields());
@@ -180,7 +218,28 @@ struct reflected_tagged final:
 };
 
 template <typename T>
-struct reflected_enum final: detail::_reflected<typename std::decay<T>::type> {
+struct reflected_binding<reflected_tagged_t, T>:
+    private detail::_reflected<typename std::decay<T>::type> {
+  T &bound;
+
+  using meta = typename reflected_tagged_t<T>::meta;
+  using typename meta::type;
+
+  operator const type &() const {
+    return bound;
+  }
+
+  // todo: do I need `value` and/or `set_value`? For fields that is incidental
+  // due to possibility of having bit fields
+
+  constexpr auto fields() const {
+    return meta::fields(bound);
+  }
+};
+
+template <typename T>
+struct reflected_enum_t final:
+    detail::_reflected<typename std::decay<T>::type> {
   static_assert(std::is_enum<T>::value, "Type is not a enum");
   static_assert(is_reflected<T>::value,
     "Type was not reflected (Calling outside a reflected scope?)");
@@ -189,59 +248,103 @@ struct reflected_enum final: detail::_reflected<typename std::decay<T>::type> {
   static_assert(reflected_entity::enumeration == meta::entity(),
     "Inconcistent reflection");
 
-  using meta::type; //< yields T
+  using typename meta::type; //< yields T
   using meta::name; //< static constexpr const char(&[N]) name()`
   using meta::enumerators; //< yields std::array of pair<type, const char*>
 };
 
 template <typename T>
+struct reflected_binding<reflected_enum_t, T>:
+    private detail::_reflected<typename std::decay<T>::type> {
+  T &bound;
+
+  using meta = typename reflected_tagged_t<T>::meta;
+  using typename meta::type;
+
+  constexpr auto enumerators() const {
+    return meta::enumerators();
+  }
+};
+
+template <typename T>
 using reflected_t = typename std::conditional<reflected_entity::tagged
     == detail::_reflected<T>::entity(),
-  reflected_tagged<T>,
-  reflected_enum<T>>::type;
+  reflected_tagged_t<T>,
+  reflected_enum_t<T>>::type;
 
-// todo: comment about why 'set_value' is even needed:
-// C++ doesn't allow references or pointers to bit fields. But setting them via
-// a 'visitor' is ok.
+// lvalue versions (todo: proper wording)
 
-// C++11 adapter, because generic lambdas (with `auto` arg) are only available
-// since C++14
 template <typename T>
-struct set_value {
-  virtual void operator()(T &&) const = 0;
-  virtual void operator()(const T &) const = 0;
+constexpr auto reflected_tagged() -> reflected_tagged_t<T> {
+  return {};
+}
 
-  protected:
-  ~set_value() = default;
+template <typename T>
+constexpr auto reflected_tagged(T &t)
+  -> reflected_binding<reflected_tagged_t, T> {
+  return {t};
+}
+
+template <typename T>
+constexpr auto reflected_enum() -> reflected_enum_t<T> {
+  return {};
+}
+
+// todo: Do I actually need a reflected_binding<reflected_enum_t, T> for enum?
+// As of now I don't see a reason for it or a use case
+template <typename T>
+constexpr auto reflected_enum(T &t) -> reflected_binding<reflected_enum_t, T> {
+  return {t};
+}
+
+// polymorphic lvalue accessors
+template <typename T>
+constexpr auto reflected() -> reflected_t<T> {
+  return {};
+}
+
+template <typename T>
+constexpr auto reflected(T &t) -> reflected_binding<reflected_t, T> {
+  return {t};
+}
+
+// utility
+
+// todo: forward-friendly
+template <template <typename...> class Variant, typename... T>
+constexpr std::array<Variant<T...>, sizeof...(T)> tuple_to_array(
+  std::tuple<T...> t) {
+  // fixme: auto lambda argument is C++14
+  return detail::apply(
+    [](auto... t) -> std::array<Variant<T...>, sizeof...(T)> {
+      return {std::move(t)...};
+    },
+    std::move(t));
+}
+
+struct type_info_t {
+  const char *name; //< refactorme: I need std::string_view-like type for this
+
+  // todo: namespaces?
 };
 
-template <typename Ref,
-  typename Field,
-  typename ValueT =
-    typename std::decay<decltype(Field::get_value(std::declval<Ref>()))>::type>
-struct _set_value final: set_value<ValueT> {
-  // todo: static_assert Field belongs to Ref
-
-  // somewhere such error should be reported in a clear manner
-  // todo: static_assert(std::is_const<Ref>() && !Field::is_mutable, "")
-  Ref &_r;
-
-  // note: this is necessary because of inheritance
-  constexpr _set_value(Ref &r): _r(r) {
-  }
-
-  template <typename V>
-  void operator()(V &&v) const {
-    Field::set_value(_r, std::forward<V>(v));
-  }
-
-  void operator()(ValueT &&v) const override {
-    return Field::set_value(_r, std::move(v));
-  }
-
-  void operator()(const ValueT &v) const override {
-    return Field::set_value(_r, v);
-  }
-};
+// convenience adapter to get type info from Variant of field bindings. Example:
+// for (auto f : omni::reflected(t).fields()) {
+// std::cout << omni::type_info(f).name << '\n'; //< Polymorphic access without
+// calling std::visit
+// }
+template <template <typename...> class Variant, typename... T>
+constexpr type_info_t type_info(const Variant<T...> &t) {
+  // todo: constraints on T: it might be a field's generated Meta, or a
+  // reflected Binding. However, it shouldn't matter, since both of them define
+  // `meta::name()`
+  return customization::visit<Variant>{}(
+    [](const auto &t) -> type_info_t {
+      return {
+        t.name(),
+      };
+    },
+    t);
+}
 
 } // namespace omni
